3505c9b8c4de2f799ee8d105ef4c1183e7a2d782
zeina-se <zeina.25@gmail.com>
2023-08-01 06:21:59 +0300

    added migrations

---
+++ backend/database/migrations/2023_08_01_030639_create_cart_details_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cart_details', function (Blueprint $table) {
+            $table->integer("product_id");
+            $table->integer("cart_id");
+            $table->integer("quantity");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cart_details');
+    }
+};
